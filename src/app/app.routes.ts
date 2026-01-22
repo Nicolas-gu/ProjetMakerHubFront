5bd280c32f39388d84006228ec8feafb6645a7f2
Nicolas <nicolas.gueb.dev@gmail.com>
2026-01-22 23:17:29 +0100

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "home", component: Home},
+    { path: "recipe", 
+        loadComponent: () => import("./components/recipe/recipe-list/recipe-list").then(r => r.RecipeList)
+    },
+    { path: "recipe-add", 
+        loadComponent: () => import("./components/recipe/recipe-add/recipe-add").then(r => r.RecipeAdd)
+    },
+    { path: "", redirectTo: 'home', pathMatch: 'full' },
+];
