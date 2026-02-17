ce57884cf68e4d06d3741465fc1c5ad13297ff04
Nicolas G <nicolas.gueb.dev@gmail.com>
2026-02-17 15:24:41 +0100

    techno

--- src/app/components/layout/navbar/navbar.ts
+++ src/app/components/layout/navbar/navbar.ts
@@ -4,16 +4,18 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { TokenService } from '../../../core/services/token-service';
 import { AuthService } from '../../../core/services/auth-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterLink, MatIconModule, MatMenuModule],
+  imports: [RouterLink, MatIconModule, MatMenuModule, CommonModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.css',
 })
 export class Navbar {
 
   private _authService = inject(AuthService)
+  public _tokenService = inject(TokenService)
 
   logout(){
     this._authService.logout();
